ba6511d613c90fc731ac3c5ad1c55f89902c7d66
BKMH <wjsqhrhks@gmail.com>
2019-08-27 08:34:24 +0900

    test

---
+++ app.js
@@ -0,0 +1,81 @@
+/*
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// disable view engine
+//app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// static path disable
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+*/
+
+require('dotenv').config()
+
+import createError from 'http-errors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import indexRouter from './routes/index'
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use('/', indexRouter)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = process.env.NODE_ENV === 'development' ? err : {}
+return res.status(err.status || 500)
+  .json(res.locals.error)
+})
+
+// bin/www 를 그대로 사용하기 위해서 예외적으로 commonJs 문법을 적용
+module.exports = app
+
+// es6으로 생성된 소스는 babel을 통해 해당 실행환경에 맞는 문법으로 자동변경됨
---
+++ controllers/index.controller.js
@@ -0,0 +1,8 @@
+// es6
+const index = (req, res, next) => res.json({
+    message: 'Hello World!'
+  })
+  
+export {
+    index
+}
